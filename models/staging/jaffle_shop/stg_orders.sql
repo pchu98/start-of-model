45c588ff9720661f25d935524f4a8e5e835dd7cb
polly.chu@baruchmail.cuny.edu <polly.chu@baruchmail.cuny.edu>
2021-11-20 05:46:59 +0000

    e-i

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,14 +1,12 @@
-with orders as (
-    
-    select
+select
     id as order_id,
     user_id as customer_id,
     order_date,
     status
 from {{ source('shop', 'jaffle_shop_orders') }}
 
-)
+{{ limit_data_in_dev(column_name = 'order_date', dev_days_of_data = 1400)}}
 
-select * from orders
 
---returned, completed, shipped, placed
+
+
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,7 +4,7 @@ select
     paymentmethod as payment_method,
     status,
 
-    amount / 100 as amount,
+    {{ cents_to_dollars('amount' , 4) }} as amount,
     created as created_at
 
-from shop.stripe_payment 
+from {{ source('shop', 'stripe_payment') }}
